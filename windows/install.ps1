055ddcd76bd93265b6f794d7d91f3b0d0079fb35
Piotr Konkol <piotrkonkol@protonmail.com>
2022-01-17 18:29:13 +0100

    More complete list of packages, sorted by utility

---
+++ windows/install.ps1
@@ -0,0 +1,6 @@
+#Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+#$env:path = $env:Path + ';%ALLUSERSPROFILE%\chocolatey\bin'
+choco install -y $(cat choco_packages)
+
+wsl --install
+wsl --install -d Debian
