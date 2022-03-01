eabd1a1dc6a58646b54c5ad82271086e74a4a7ee
Piotr Konkol <piotrkonkol@protonmail.com>
2022-03-01 18:46:32 +0100

    choco packages update & obsidian backup in ps1

--- windows/install.ps1
+++ windows/install.ps1
@@ -1,5 +1,6 @@
 #Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 #$env:path = $env:Path + ';%ALLUSERSPROFILE%\chocolatey\bin'
+Enable-WindowsOptionalFeature -Online -FeatureName $("Microsoft-Hyper-V", "Containers") -All
 choco install -y $(cat choco_packages)
 choco install -y python --version 3.8.6
 choco install -y python --version 3.9.5
